028439974ec02715bd2228b6ef24bfcff1820972
Izy 2 nv <izy2nv@Izys-MacBook-Pro.local>
2016-09-28 15:17:11 -0400

    added date picker to app.js and removed it from index.html

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,17 @@
 var app = angular.module("app", []); // defines d MODULE for d application (cos of d [] which will contain dependencies)
+$( function() {
+  from = $( "#from" ).datepicker({
+    dateFormat: "yy-mm-dd",
+    changeMonth: true,
+    changeYear: true,
+    numberOfMonths: 1,
+    maxDate: new Date()
+  });
+  to = $( "#to" ).datepicker({
+    dateFormat:"yy-mm-dd",
+    changeMonth: true,
+    changeYear: true,
+    numberOfMonths: 1,
+    maxDate: new Date()
+  });
+});
